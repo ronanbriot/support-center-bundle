05d451f8d48a12afb0724addc985b13e235e336b
ronanb <34341375+ronanbriot@users.noreply.github.com>
2024-10-18 23:38:29 +0200

    Fix deprecated

--- UVDeskSupportCenterBundle.php
+++ UVDeskSupportCenterBundle.php
@@ -4,10 +4,18 @@ namespace Webkul\UVDesk\SupportCenterBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Webkul\UVDesk\SupportCenterBundle\DependencyInjection\SupportCenterExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 
 class UVDeskSupportCenterBundle extends Bundle
 {
-    public function getContainerExtension()
+    /**
+     * Returns the bundle's container extension.
+     *
+     * @return ExtensionInterface|null
+     *
+     * @throws \LogicException
+     */
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new SupportCenterExtension();
     }
